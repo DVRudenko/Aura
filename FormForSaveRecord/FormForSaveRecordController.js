43d675f829e48f156c9e3f85c828fb6cbce6434d
Dmitry Rudenko <51669429+DVRudenko@users.noreply.github.com>
2021-12-10 08:43:53 +0300

    Update FormForSaveRecordController.js

--- FormForSaveRecord/FormForSaveRecordController.js
+++ FormForSaveRecord/FormForSaveRecordController.js
@@ -1,12 +1,12 @@
 ({
   saveAcc : function(component, event) {
-    var newAcc = component.get("v.acc");
-    var action = component.get("c.saveAccount");
+    let newAcc = component.get("v.acc");
+    let action = component.get("c.saveAccount");
     action.setParams({
       "acc":newAcc
     });
     action.setCallback(this, function(response) {
-      var state = response.getState();
+      let state = response.getState();
       if (state === "SUCCESS") {
         component.set("v.accId", response.getReturnValue().Id);
         component.set("v.acc", response.getReturnValue());
