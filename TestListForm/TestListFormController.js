254c7c48c972354665f3180c624c9186f0640e55
Dmitry Rudenko <51669429+DVRudenko@users.noreply.github.com>
2021-12-10 08:44:20 +0300

    Update TestListFormController.js

--- TestListForm/TestListFormController.js
+++ TestListForm/TestListFormController.js
@@ -1,8 +1,8 @@
 ({
   getAccounts : function(component, event) {
-    var action = component.get("c.getListAccounts");
+    let action = component.get("c.getListAccounts");
     action.setCallback(this, function(response) {
-      var state = response.getState();
+      let state = response.getState();
       if (state === "SUCCESS") {
         //  var stringItems = response.getReturnValue();
         component.set("v.accounts", response.getReturnValue());
